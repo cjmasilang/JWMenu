179260eb88b938e48c76f638be921d2736906dca
Cedric Jay <119407422+cjmasilang@users.noreply.github.com>
2022-12-22 21:16:39 +0800

    Add files via upload

---
+++ access/session/restriction.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Access Denied</title>
+</head>
+<body>
+	<?php 
+	session_start();
+	 ?>
+<h1>You have no permission to view this page. </h1>
+</h2>Please contact technical support for more information.</h2>
+</body>
+</html>
